6b7c29e6f023e4fa43d5e5e8017b4143269e7426
Armando Di Santo <armando.di.santo@hotmail.com>
2024-05-09 00:15:03 +0200

    selfwork-operatori-2

---
+++ main.js
@@ -0,0 +1,10 @@
+// Variabili
+let annoCorrente = 2024;
+let annoDiNascita = 1997;
+
+// operazioni
+let eta = annoCorrente - annoDiNascita;
+let cento = 100 - eta;
+
+// risultato
+console.log("Hai " + eta + " anni e ti mancano " + cento + " anni per compierne 100");
